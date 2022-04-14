6d9619cbc2e1e76cf8230838b6229c36b4a3fe14
turpen2319 <austin-turpen@outlook.com>
2022-04-14 14:32:20 -0700

    adds .env and moves django secret key

--- creative_marketplace/settings.py
+++ creative_marketplace/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-49@byz8vnin7&=pe-%i0098^6$d%bq8kfk*0^3%lddlo9y@s)4'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
