c229fa5109162a3f557fbc1a803039032cbfd21c
ricky barrera <rickybarrrera0@gmail.com>
2022-04-16 13:32:04 -0700

    adds Product Create functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('product/', views.ProductList.as_view(), name="products_index"),
+    path('products/', views.ProductList.as_view(), name="products_index"),
+    path('products/create/', views.ProductCreate.as_view(), name='products_create'),
 
 ]
