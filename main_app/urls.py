1e64310f04b3c09dadda40d9a8eff31b758ac847
OscarFelixEscobar <oscarfelixescobar@gmail.com>
2022-04-16 14:55:03 -0700

    Adds detail view

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('products/', views.ProductList.as_view(), name="products_index"),
     path('products/create/', views.ProductCreate.as_view(), name='products_create'),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name='products_detail')
 
 ]
