e051f979fd9b86d42e538fdfc97de317203ba44e
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-04-21 12:34:43 -0600

    trying to figure out the search bar

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,18 @@
-from django.forms import ModelForm
-from .models import Comment
+from django.forms import ModelForm, ChoiceField
+from .models import Comment, Product, Tag
  
 class CommentForm(ModelForm):
  class Meta:
    model = Comment
    fields = ['text']
+
+class SearchForm(ModelForm):
+  class Meta:
+    model = Product
+    fields = ['tags']
+  def __init__(self, **kwargs):
+    super(SearchForm, self).__init__(**kwargs)
+    self.fields['tags'].required = False
+   
+
+
