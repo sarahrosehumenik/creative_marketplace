05b3afd7927c0472ae58b8c3ff79b57a6fb5f35b
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-06-15 11:46:43 -0600

    styling

---
+++ main_app/static/css/JS/javascript.js
@@ -0,0 +1,9 @@
+
+let about = document.getElementById("about")
+
+let body = document.querySelector("body")
+
+about.addEventListener("click", myClick)
+function myClick() {
+ body.style.backgroundColor = "red";
+}
