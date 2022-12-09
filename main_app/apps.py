91a716d1051bdbbd8bba930dd9dcbeb371960b4f
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-12-08 17:44:39 -0700

    complete refactor begins

--- main_app/apps.py
+++ main_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_app'
+    
+
+class ErpConfig(AppConfig):
+    default = False
