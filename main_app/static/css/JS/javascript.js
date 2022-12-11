9cdfaa8a49991f3cd1126c01ac084cc00626cdc8
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-12-10 20:59:20 -0700

    not logged in page

--- main_app/static/css/JS/javascript.js
+++
@@ -1,9 +0,0 @@
-
-let about = document.getElementById("about")
-
-let body = document.querySelector("body")
-
-about.addEventListener("click", myClick)
-function myClick() {
- body.style.backgroundColor = "red";
-}
